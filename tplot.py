61d4f3fc5c174ac1d5b04cee663e33021a70097b
TimotejZavski <timotej.zavski@tutanota.com>
2024-01-14 17:13:26 +0100

    nov
    nov

--- tplot.py
+++ tplot.py
@@ -1,42 +1,42 @@
 
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
-# Load data
-candy_data = pd.read_json('/Users/timzav/Desktop/test/test/wwwroot/uploads/candy.json')
+# Load the data from the JSON file
+data_path = '/Users/timzav/Desktop/test/test/wwwroot/uploads/penguins.json'
+df = pd.read_json(data_path)
 
-# Chart 1: Bar plot of win percentage by chocolate presence
-chocolate_win = candy_data.groupby('chocolate')['winpercent'].mean().reset_index()
-plt.figure(figsize=(10, 6))
-sns.barplot(x='chocolate', y='winpercent', data=chocolate_win)
-plt.title('Win Percentage by Chocolate Presence')
-plt.savefig('/Users/timzav/Desktop/test/test/wwwroot/images/chocolate_win_percent.png')
+# Create plots directory if it does not exist
+images_dir = '/Users/timzav/Desktop/test/test/wwwroot/images/'
+if not os.path.exists(images_dir):
+    os.makedirs(images_dir)
+
+# Example plots
+
+# Histogram of culmen length
+plt.figure(figsize=(8, 6))
+sns.histplot(df['culmen_length_mm'])
+plt.title('Histogram of Culmen Length')
+plt.xlabel('Culmen Length (mm)')
+plt.ylabel('Count')
+culmen_length_hist_path = os.path.join(images_dir, 'culmen_length_hist.png')
+plt.savefig(culmen_length_hist_path)
 plt.close()
 
-# Chart 2: Histogram of sugar percentage
-plt.figure(figsize=(10, 6))
-sns.histplot(candy_data['sugarpercent'], kde=True, bins=10)
-plt.title('Sugar Percentage Distribution')
-plt.savefig('/Users/timzav/Desktop/test/test/wwwroot/images/sugar_percent_distribution.png')
+# Scatter plot of culmen depth vs flipper length
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x='culmen_depth_mm', y='flipper_length_mm', hue='sex', data=df)
+plt.title('Culmen Depth vs. Flipper Length by Sex')
+plt.xlabel('Culmen Depth (mm)')
+plt.ylabel('Flipper Length (mm)')
+culmen_depth_flipper_scatter_path = os.path.join(images_dir, 'culmen_depth_flipper_scatter.png')
+plt.savefig(culmen_depth_flipper_scatter_path)
 plt.close()
 
-# Chart 3: Scatter plot of win percent vs. price percent
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x='pricepercent', y='winpercent', hue='chocolate', data=candy_data)
-plt.title('Win Percent vs. Price Percent')
-plt.savefig('/Users/timzav/Desktop/test/test/wwwroot/images/win_vs_price.png')
+# Pairplot
+sns.pairplot(df, hue='sex')
+pairplot_path = os.path.join(images_dir, 'pairplot.png')
+plt.savefig(pairplot_path)
 plt.close()
-
-# Chart 4: Box plot of win percent by bar type
-plt.figure(figsize=(10, 6))
-sns.boxplot(x='bar', y='winpercent', data=candy_data)
-plt.title('Win Percent by Bar Type')
-plt.savefig('/Users/timzav/Desktop/test/test/wwwroot/images/win_percent_by_bar.png')
-plt.close()
-
-# Chart 5: Pie chart of candy types with and without peanuts or almonds
-peanuts_almonds_counts = candy_data['peanutyalmondy'].value_counts()
-fig = px.pie(values=peanuts_almonds_counts, names=peanuts_almonds_counts.index, title='Candy Types with and without Peanuts/Almonds')
-fig.write_image('/Users/timzav/Desktop/test/test/wwwroot/images/peanuts_almonds_pie_chart.png')
